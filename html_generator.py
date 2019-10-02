f7667f18179fe1e34a5b40e31639d26ffcb43dfa
Danilo Cunha <danilokage@gmail.com>
2019-10-01 23:36:28 -0300

    Add logic to html generator

--- html_generator.py
+++ html_generator.py
@@ -1,12 +1,35 @@
+import os
+
+
+def get_full_path(series, chapter):
+    main_path = './tmp/'
+    series_dir = series + '/'
+
+    return main_path + series_dir + str(chapter)
+
+
 doctype = "<!DOCTYPE html><html>"
 head_and_css = "<head>" \
-               "    <link rel='stylesheet' href='../css/manga.css'>" \
+               "    <link rel='stylesheet' href='../../css/manga.css'>" \
                "</head>"
 body_start = "<body>"
 
-# TODO add logic to insert every image in a loop
-content = 'IMGS GO HERE'
+content = "<div id='imgs_container'>"
+
+# TODO refactor these variables (maybe a def?)
+series = 'one-piece'
+chapter_images = sorted(os.listdir(get_full_path('one-piece', 813)))
+for image in chapter_images:
+    img_element = "<img src='../../" + get_full_path('one-piece', 813) + '/' + image + "'>"
+    content = content + img_element
+content = content + "</div>"
+
 
 body_end = "</body></html>"
 
 html_to_save = doctype + head_and_css + body_start + content + body_end
+
+html_file = open('./tmp/html/tmp.html', 'w')
+html_file.write(html_to_save)
+html_file.close()
+
