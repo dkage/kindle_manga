3621ad4b9797be44460420344ff47b7ec6414684
Danilo Cunha <danilokage@gmail.com>
2019-10-14 19:23:59 -0300

    Add comment, add TODO

--- kindlegen/html_generator.py
+++ kindlegen/html_generator.py
@@ -1,10 +1,11 @@
 import os
 from functions import get_full_path
 
-
+# GENERATES HTML
 # TODO this needs to be changed into functions or class
 doctype = "<!DOCTYPE html>" \
           "     <html>"
+# TODO CSS needs rework to improve image to screen size ratio (too many blank spaces) more trial and error needed
 head_and_css = "<head>" \
                "    <link rel='stylesheet' href='../../css/manga.css'>" \
                "</head>"
@@ -19,9 +20,8 @@ for image in chapter_images:
     img_element = "<img src='../../" + get_full_path('one-piece', 813) + '/' + image + "'>"
     content = content + img_element
 content = content + "</div>"
-
-
 body_end = "</body></html>"
+
 
 html_to_save = doctype + head_and_css + body_start + content + body_end
 
