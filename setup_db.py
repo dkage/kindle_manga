cbb3de191723da599b037a1062b1498264aa1eff
Danilo Cunha <danilokage@gmail.com>
2019-10-30 22:53:46 -0300

    Add comments and identation to setup to create DATABASE

--- setup_db.py
+++ setup_db.py
@@ -1,21 +1,21 @@
 import configparser
 import psycopg2
 from psycopg2 import sql
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT # <-- ADD THIS LINE
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
 # TODO create class for database connection
 config = configparser.ConfigParser()
 config.read('config/postgres_conn.ini')
 
-
+# User to be passed from ini file needs to have CREATE DATABASE permissions
 con = psycopg2.connect(dbname='postgres',
                        user=config['postgres']['user'],
                        host=config['postgres']['host'],
                        password=config['postgres']['password'])
+con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)  # This is needed to sql create database, as it needs autocommit
 
-con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) # <-- ADD THIS LINE
+
 cur = con.cursor()
-
 cur.execute(sql.SQL("CREATE DATABASE kindle_manga;"))
 
