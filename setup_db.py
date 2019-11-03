673d299d2967a28962d5b175a876ae7bcfcdf0f8
Danilo Cunha <danilokage@gmail.com>
2019-11-02 23:50:44 -0300

    Add new TODOs for better setup automation

--- setup_db.py
+++ setup_db.py
@@ -8,13 +8,19 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 config = configparser.ConfigParser()
 config.read('config/postgres_conn.ini')
 
-# User to be passed from ini file needs to have CREATE DATABASE permissions
-con = psycopg2.connect(dbname='postgres',
-                       user=config['postgres']['user'],
-                       host=config['postgres']['host'],
-                       password=config['postgres']['password'])
-con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)  # This is needed to sql create database, as it needs autocommit
+# TODO Check postgres accessible
+# TODO Check postgres_admin
+# TODO If database not exists ? create : nothing
+# TODO If user not exists ? create : nothing
+# TODO Set user as owner of new database
 
+
+# User to be passed from ini file needs to have CREATE DATABASE permissions
+con = psycopg2.connect(dbname=config['postgres_admin']['database'],
+                       user=config['postgres_admin']['user'],
+                       host=config['postgres_admin']['host'],
+                       password=config['postgres_admin']['password'])
+con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)  # This is needed to sql create database, as it needs autocommit
 
 cur = con.cursor()
 cur.execute(sql.SQL("CREATE DATABASE kindle_manga;"))
