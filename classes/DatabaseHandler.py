c3b8362010c7c6a32cea30bbb994508c18b0a31d
Danilo Cunha <danilokage@gmail.com>
2019-11-03 23:44:21 -0300

    Start creating database class variables for connections

--- classes/DatabaseHandler.py
+++ classes/DatabaseHandler.py
@@ -2,7 +2,21 @@ import configparser
 import psycopg2
 
 
-class DatabaseHandlers:
+class DatabaseHandler:
     def __init__(self):
         config = configparser.ConfigParser()
         config.read('config/postgres_conn.ini')
+        self.kindle_user = dict(config['postgres'])
+        # Checks for admin key. Not obligatory to be used as it is only used during first setup.
+        try:
+            self.admin = dict(config['postgres_admin'])
+        except KeyError:
+            self.admin = []
+
+
+    def print_users(self):
+        print(self.admin)
+        print(self.kindle_user)
+
+
+
