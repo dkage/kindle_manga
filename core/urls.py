264af49775eab4c37502bab82c3f91f714d04e6a
Danilo Cunha <danilokage@gmail.com>
2020-05-06 23:49:30 -0300

    Add new base template, add css, add bg, add views/urls

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', core.index, name='index'),
     path('index', core.index),
 
-    path('register', core.register, name='register')
+    path('register', core.register, name='register'),
+    path('login', core.login, name='login')
 ]
